82b91d8eff2838a8dba99a18c793c56107cb3ca8
OUSMANE MERIGA <ousmane.meriga12@gmail.com>
2025-07-16 08:41:32 +0100

    Suppression de la condition d'affichage pour la page de résultat de paiement, simplifiant ainsi la logique d'authentification et améliorant l'expérience utilisateur lors de la navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Pages d'authentification -->
-  <div v-if="!isAuthenticated && $route.name !== 'payment-result'" class="min-h-screen bg-slate-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div v-if="!isAuthenticated" class="min-h-screen bg-slate-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-slate-900">
         {{ currentView === 'login' ? 'Connexion' : 'Créer un compte' }}
